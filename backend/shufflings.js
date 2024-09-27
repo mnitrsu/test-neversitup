517069c1b257f4ac5c1b3019cacd4600510fe6fb
man <mnitrsu@gmail.com>
2024-09-27 20:13:15 +0700

    [build] shufflings v2

--- backend/shufflings.js
+++ backend/shufflings.js
@@ -2,23 +2,29 @@ const readlineSync = require("readline-sync");
 
 console.clear();
 
-// const input = readlineSync.question("Input something? -> ");
-const input = "aabb";
-const input_arr = input.split("");
-let output = [];
-let temp = [...input_arr];
-for (i = 0; i < temp.length; i++) {
-  console.log(i);
-  if (i > 0) {
-    temp.push(temp.splice(0, 1));
-    console.log(temp);
+while (true) {
+  let uniq = "";
+  console.clear();
+  const input = readlineSync.question("Input something? -> ");
+  if (input.length > 1) {
+    const input_arr = input.split("");
+    let output = [];
+    let output_rev = [];
+    let temp = [...input_arr];
+    for (position = 0; position < input_arr.length - 1; position++) {
+      for (i = 0; i < temp.length; i++) {
+        temp.push(temp.splice(position, 1).toString());
+        output.push(temp.join(""));
+        output_rev.push(temp.reverse().join(""));
+      }
+    }
+    uniq = [...new Set([...output, ...output_rev])].filter((item) => item.length > 1).sort();
+  } else {
+    uniq = input;
   }
-  output.push(temp.join(""));
+  console.log(`\n======`);
+  console.log(`\nWith input '${input}':`);
+  console.log(`Your function should return: ${uniq}`);
+  const pause = readlineSync.question(`\n---
+    press 'enter' to restart\n`);
 }
-console.log(`With input '${input}'`);
-console.log(`Your function should return: ${output}`);
-
-// aabb
-// abab
-// abba
-// aabb
