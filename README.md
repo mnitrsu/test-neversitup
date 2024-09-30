c7ea784d59397683246afd96712becc4cd05d952
man <mnitrsu@gmail.com>
2024-09-30 14:14:43 +0700

    [docs] readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# test-neversitup
+
+with node v18.17.1
+
+backend:
+https://github.com/mnitrsu/test-neversitup/blob/main/backend/README.md#test-neversitup-backend
+
+frontend:
+$${\color{red}didn't \space complete \space yet}$$
--- frontend/src/pages/todo.tsx
+++ frontend/src/pages/todo.tsx
@@ -2,6 +2,7 @@ import Task from "@/components/Task";
 import Main from "@/containers/Main";
 import { getTodos } from "@/services/todo";
 import { ITodo } from "@/types/ITodo";
+import { IconCalendar, IconCheck, IconPlus } from "@tabler/icons-react";
 import { useEffect, useState } from "react";
 
 const TodoPage = () => {
@@ -19,6 +20,17 @@ const TodoPage = () => {
         {todos.map(({ id, title, completed }, index) => {
           return <Task {...{ id, title, completed }} />;
         })}
+      </div>
+      <div className="absolute bottom-3 w-full flex justify-evenly mb-4 items-end">
+        <div className="w-9 h-9 rounded-full bg-purple-500 flex items-center justify-center drop-shadow-lg">
+          <IconCheck color="white" size={18} />
+        </div>
+        <div className="w-12 h-12 rounded-full bg-white flex items-center justify-center drop-shadow-lg">
+          <IconCalendar color="red" size={28} />
+        </div>
+        <div className="w-9 h-9 rounded-full bg-sky-500 flex items-center justify-center drop-shadow-lg">
+          <IconPlus color="white" size={18} />
+        </div>
       </div>
     </Main>
   );
